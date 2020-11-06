1cab47b34c6ec5f2a76fe4091aac759467e94007
TurboPikk <74012943+TurboPikk@users.noreply.github.com>
2020-11-06 16:07:07 +0100

    Add files via upload

--- Project 2 - Questions.py
+++ Project 2 - Questions.py
@@ -1,80 +1,93 @@
-q1string = "Q1. What is the capital of Norway?"
-q2string = "Q2.What is the currency of Norway?"
-q3string = "Q3.What is the largest city in Norway?"
-q4string = "Q4.When is constitution day (the national day) of Norway?"
-q5string = "Q5.What color is the background of the Norwegian flag?"
-q6string = "Q6.How many countries does Norway border?"
-q7string = "Q7.What is the name of the university in Trondheim?"
-q8string = "Q8.How long is the border between Norway and Russia?"
-q9string = "Q9.Where in Norway is Stavanger?"
-q10string = "Q10.From which Norwegian city did the world famous composer Edvard Grieg come?"
+q1 = "Q1. What is the capital of Norway?"
+q2 = "Q2.What is the currency of Norway?"
+q3 = "Q3.What is the largest city in Norway?"
+q4 = "Q4.When is constitution day (the national day) of Norway?"
+q5 = "Q5.What color is the background of the Norwegian flag?"
+q6 = "Q6.How many countries does Norway border?"
+q7 = "Q7.What is the name of the university in Trondheim?"
+q8 = "Q8.How long is the border between Norway and Russia?"
+q9 = "Q9.Where in Norway is Stavanger?"
+q10 = "Q10.From which Norwegian city did the world famous composer Edvard Grieg come?"
 
-q1dic = {
+o1 = {
     "a" : "Bergen",
     "b" : "Oslo",
     "c" : "Stavanger",
-    "d" : "Trondheim
+    "d" : "Trondheim"
 }
 
-q2dic = {
+o2 = {
     "a" : "Euro",
     "b" : "Pound",
     "c" : "Krone",
     "d" : "Deutsche Mark"
 }
 
-q3dic = {
+o3 = {
     "a" : "Oslo",
     "b" : "Stavanger",
     "c" : "Bergen",
-    "d" : "Trondheim
+    "d" : "Trondheim"
 }
 
-q4dic = {
+o4 = {
     "a" : "27thMay",
     "b" : "17thMay",
     "c" : "17thApril",
-    "d" : "27thApril
+    "d" : "27thApril"
 }
 
-q5dic = {
+o5 = {
     "a" : "Red",
     "b" : "White",
     "c" : "Blue",
-    "d" : "Yellow
+    "d" : "Yellow"
 }
 
-q6dic = {
+o6 = {
     "a" : "1",
     "b" : "2",
     "c" : "3",
-    "d" : "4
+    "d" : "4"
 }
 
-q7dic= {
+o7 = {
     "a" : "UiS",
     "b" : "UiO",
     "c" : "NMBU",
     "d" : "NTNU"
 }
 
-q8dic = {
+o8 = {
     "a" : "96 km",
     "b" : "196 km",
     "c" : "296 km",
-    "d" : "396 km
+    "d" : "396 km"
 }
 
-q9dic = {
+o9 = {
     "a" : "North",
     "b" : "South",
     "c" : "South-west",
-    "d" : "South-east
+    "d" : "South-east"
 }
 
-q10dic = {
+o10 = {
     "a" : "Oslo",
     "b" : "Bergen",
     "c" : "Stavanger",
-    "d" : "Tromsø
+    "d" : "Tromsø"
 }
+
+solution = ["b", "c", "a", "b", "a", "c", "d", "b", "c", "b"]
+
+def options(dictionary):
+    for element in dictionary:
+        print(element, ":", dictionary[element])
+    return
+
+
+
+y = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10]
+x = [o1, o2, o3, o4, o5, o6, o7, o8, o9, o10]
+
