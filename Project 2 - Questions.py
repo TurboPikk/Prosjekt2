87b9a978111382f1d98cb4dc2aa46ac5cd9a24b8
TurboPikk <74012943+TurboPikk@users.noreply.github.com>
2020-11-06 15:26:07 +0100

    Add files via upload

---
+++ Project 2 - Questions.py
@@ -0,0 +1,80 @@
+q1string = "Q1. What is the capital of Norway?"
+q2string = "Q2.What is the currency of Norway?"
+q3string = "Q3.What is the largest city in Norway?"
+q4string = "Q4.When is constitution day (the national day) of Norway?"
+q5string = "Q5.What color is the background of the Norwegian flag?"
+q6string = "Q6.How many countries does Norway border?"
+q7string = "Q7.What is the name of the university in Trondheim?"
+q8string = "Q8.How long is the border between Norway and Russia?"
+q9string = "Q9.Where in Norway is Stavanger?"
+q10string = "Q10.From which Norwegian city did the world famous composer Edvard Grieg come?"
+
+q1dic = {
+    "a" : "Bergen",
+    "b" : "Oslo",
+    "c" : "Stavanger",
+    "d" : "Trondheim
+}
+
+q2dic = {
+    "a" : "Euro",
+    "b" : "Pound",
+    "c" : "Krone",
+    "d" : "Deutsche Mark"
+}
+
+q3dic = {
+    "a" : "Oslo",
+    "b" : "Stavanger",
+    "c" : "Bergen",
+    "d" : "Trondheim
+}
+
+q4dic = {
+    "a" : "27thMay",
+    "b" : "17thMay",
+    "c" : "17thApril",
+    "d" : "27thApril
+}
+
+q5dic = {
+    "a" : "Red",
+    "b" : "White",
+    "c" : "Blue",
+    "d" : "Yellow
+}
+
+q6dic = {
+    "a" : "1",
+    "b" : "2",
+    "c" : "3",
+    "d" : "4
+}
+
+q7dic= {
+    "a" : "UiS",
+    "b" : "UiO",
+    "c" : "NMBU",
+    "d" : "NTNU"
+}
+
+q8dic = {
+    "a" : "96 km",
+    "b" : "196 km",
+    "c" : "296 km",
+    "d" : "396 km
+}
+
+q9dic = {
+    "a" : "North",
+    "b" : "South",
+    "c" : "South-west",
+    "d" : "South-east
+}
+
+q10dic = {
+    "a" : "Oslo",
+    "b" : "Bergen",
+    "c" : "Stavanger",
+    "d" : "Tromsø
+}
